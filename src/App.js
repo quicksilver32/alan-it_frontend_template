97669cb2ed405c172d5685186f67aac29fca7e0c
Quicksilver32 <ameliya9@mail.ru>
2022-07-13 15:20:46 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Graph from "./Components/Graph";
+
+function App() {
+  return (
+    <Graph/>
+  );
+}
+
+export default App;
---
+++ src/Components/Graph.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Graph = () => {
+  return <p>Здесь должны быть графики</p>
+}
+
+export default Graph
