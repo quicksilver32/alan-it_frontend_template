24e51007c02df59e4c77617d8e5a87b17a8c4952
Quicksilver32 <ameliya9@mail.ru>
2022-07-21 15:57:54 +0300

    add data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
 import Graph from "./Components/Graph";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <Graph/>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8080/api/data")
+      .then((response) => response.json())
+      .then((result) => setData(result));
+  }, []);
+  return <Graph data={data} />;
 }
 
 export default App;
--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef } from "react";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-const Graph = () => {
-  return <p>Здесь должны быть графики</p>
-}
+am4core.useTheme(am4themes_animated);
 
-export default Graph
+const Graph = ({ data }) => {
+  const chart = useRef(null);
+
+  useLayoutEffect(() => {
+    let x = am4core.create("chartdiv", am4charts.XYChart);
+
+    x.paddingRight = 20;
+
+    let finalData = [];
+
+    data.splice(1, 1);
+    data.forEach((item, index) => {
+      if (index === 0) {
+        finalData.push({
+          date: item.year + "-" + item.month,
+          year: item.year,
+          month: item.month,
+          sales: parseInt(item.sales),
+        });
+        return;
+      }
+      if (
+        item.year !== finalData[finalData.length - 1].year ||
+        item.month !== finalData[finalData.length - 1].month
+      ) {
+        finalData.push({
+          date: item.year + "-" + item.month,
+          year: item.year,
+          month: item.month,
+          sales: parseInt(item.sales),
+        });
+      } else {
+        finalData[finalData.length - 1].sales += parseInt(item.sales);
+      }
+    });
+
+    x.data = finalData;
+
+    let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+    let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.minWidth = 35;
+
+    let series = x.series.push(new am4charts.LineSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "sales";
+    series.tooltipText = "{valueY}";
+    x.cursor = new am4charts.XYCursor();
+
+    let scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(series);
+    x.scrollbarX = scrollbarX;
+
+    chart.current = x;
+
+    return () => {
+      x.dispose();
+    };
+  }, [data]);
+
+  return <div id="chartdiv" style={{ width: "100%", height: "500px" }} />;
+};
+
+export default Graph;
